e0d26a48cd147992dffad9a376bfc3e099a6e3cb
Ziyue Meng <z.y.meng@berkeley.edu>
2019-09-05 11:53:00 -0700

    part1.0
    first attempt seems to in inf loop

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,45 @@
 
 def sum arr
   # YOUR CODE HERE
+  sum = 0
+  i = 0
+  while (i < arr.length)
+    sum += arr[i]
+  end
+  return sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  fl = 0
+  sl = 0
+  i = 0
+  while (i < arr.length)
+    if (arr[i] > fl)
+      sl = fl
+      fl = arr[i]
+    elsif (arr[i] > sl)
+      sl = arr[i]
+    end
+  end
+  return fl + sl
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  arr = arr.sort
+  left = 0
+  right = arr.length - 1
+  while (left < right)
+    if (arr[left] + arr[right] == n)
+      return true
+    elsif (arr[left] + arr[right] < n)
+      left += 1
+    else
+      right -= 1
+    end
+  end
+  return false
 end
 
 # Part 2
