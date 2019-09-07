2149cb84621a0d7d340a7a631768186422c41c06
Ziyue Meng <z.y.meng@berkeley.edu>
2019-09-06 21:08:37 -0700

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -8,24 +8,21 @@ def sum arr
   i = 0
   while (i < arr.length)
     sum += arr[i]
+    i += 1
   end
   return sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
-  fl = 0
-  sl = 0
-  i = 0
-  while (i < arr.length)
-    if (arr[i] > fl)
-      sl = fl
-      fl = arr[i]
-    elsif (arr[i] > sl)
-      sl = arr[i]
-    end
+  if (arr.length == 0)
+    return 0
+  elsif (arr.length == 1)
+    return arr[0]
+  else
+    arr = arr.sort
+    return arr[arr.length - 1] + arr[arr.length - 2]
   end
-  return fl + sl
 end
 
 def sum_to_n? arr, n
@@ -49,18 +46,67 @@ end
 
 def hello(name)
   # YOUR CODE HERE
+  return 'Hello, ' + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if (s.length == 0)
+    return false
+  elsif (!(s[0] >= 'A' && s[0] <= 'Z') && !(s[0] >= 'a' && s[0] <= 'z'))
+    return false
+  end
+  a = s[0] =~ /[aeiouAEIOU]/
+  return a == nil
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  f = s.delete('01')
+  if (f == '')
+    if (s.length == 0)
+      return false
+    elsif (s.length == 1)
+      return s[0] == '0'
+    else
+      return s[s.length - 1] == '0' && s[s.length - 2] == '0'
+    end
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
+  def initialize(isbn, price)
+    if (isbn == '' || isbn.length < 5 || isbn[0..3] != 'isbn')
+      raise ArgumentError
+    end
+    tmp = isbn[4..-1].delete('0123456789-')
+    if (tmp == '' && price > 0.0)
+      @isbn = isbn
+      @price = price
+    else
+      raise ArgumentError
+    end
+  end
+
+  def isbn = (isbn)
+    if (isbn == '' || isbn.length < 5 || isbn[0..3] != 'isbn')
+      raise ArgumentError
+    end
+    tmp = isbn[4..-1].delete('0123456789-')
+    if (tmp == '')
+      @isbn = isbn
+    else
+      raise ArgumentError
+    end
+  end
+
+  def price = (price)
+    @price = price
+  end
+
 # YOUR CODE HERE
 end
